e171a662750e73244ecac8b600e0d5929f1eea2f
raina.disa <raina.disa@apps.ipb.ac.id>
2025-08-30 22:37:37 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline text-red-600'>Hello World</div>
+    </>
+  )
+}
+
+export default App
